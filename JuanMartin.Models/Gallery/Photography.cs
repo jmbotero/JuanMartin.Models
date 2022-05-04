b0082d82a45a569c63a76a13e45b45a63f3a4b1e
Juan Martin Botero <jbotero@hotmail.com>
2022-05-04 16:50:15 -0400

    photo-gallery second pass

--- JuanMartin.Models/Gallery/Photography.cs
+++ JuanMartin.Models/Gallery/Photography.cs
@@ -4,7 +4,15 @@ namespace JuanMartin.Models.Gallery
 {
     public class PhotoGraphy
     {
+
+        public enum PhysicalSource
+        {
+            negative = 0,
+            slide = 1
+        };
+
         public long Id { get; set; }
+        public PhysicalSource Source { get; set; }
         public string Title { get; set; }
         public string Path { get; set; }
         public string FileName { get; set; }

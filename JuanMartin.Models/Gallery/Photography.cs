832d55877e185d02bbe42b1cafb7fbb87bbb5f46
Juan Martin Botero <jbotero@hotmail.com>
2022-05-10 12:30:05 -0400

    photo-gallery fifth pass

--- JuanMartin.Models/Gallery/Photography.cs
+++ JuanMartin.Models/Gallery/Photography.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace JuanMartin.Models.Gallery
 {
@@ -12,18 +13,24 @@ namespace JuanMartin.Models.Gallery
         };
 
         public long Id { get; set; }
+        public int UserId { get; set; }
         public PhysicalSource Source { get; set; }
         public string Title { get; set; }
         public string Path { get; set; }
         public string FileName { get; set; }
         public int NumberOfViews { get; set; }
-        public  List<string> Keywords { get; set; }
-        public double Rank { get; private set; }
+        public  string[] Keywords { get; set; }
+        public double Rank { get; set; }
         public string Location { get; set; }
 
         public override string ToString()
         {
             return $"{System.IO.Path.Combine(Path, FileName)},{Rank},{Location}";
+        }
+
+        public void AddKeywords(string keywords)
+        {
+            Keywords = keywords.Split(',');
         }
     }
 }
